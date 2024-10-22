ec9f7c9bbd9b12aed84d021e36df969aea64a74c
Leandro Silva <leandro.jj.silva@gmail.com>
2024-10-21 22:29:23 -0300

    use of reactive form in landing page component

--- src/app/views/landing-page/landing-page.component.ts
+++ src/app/views/landing-page/landing-page.component.ts
@@ -3,17 +3,33 @@ import { Component } from '@angular/core';
 import { OutlineButtonComponent } from '../../core/outline-button/outline-button.component';
 import { FeatureService } from '../../core/services/feature.service';
 import { Feature } from '../../core/Model/feature.model';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
-    OutlineButtonComponent
+    OutlineButtonComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
+
+  email= new FormControl('');
+
+  updateEmail(){
+    
+    if(this.email.disabled){
+      this.email.setValue('Input habilitado');
+      this.email.enable();
+    }else {
+      this.email.setValue('Este input se ha deshabilitado');
+      this.email.disable();
+    }
+    
+  }
 
   constructor(private featureService: FeatureService){}
 
